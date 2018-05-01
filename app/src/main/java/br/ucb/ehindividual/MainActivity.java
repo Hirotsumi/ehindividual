623875f7dda317f099c2319c183972dffa192706
Hirotsumi <danilojartex@msn.com>
2018-04-30 22:45:21 -0300

    Finalizada tela inicial

--- app/src/main/java/br/ucb/ehindividual/MainActivity.java
+++ app/src/main/java/br/ucb/ehindividual/MainActivity.java
@@ -2,20 +2,23 @@ package br.ucb.ehindividual;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    EditText editText;
-    Button btnContinuar;
-    TextView tvTitulo;
-    int cont;
-    String nomeJogador1;
-    String nomeJogador2;
+    private ImageView imgGitHub;
+    private EditText editText;
+    private Button btnContinuar;
+    private TextView tvTitulo;
+    private int cont;
+    private String nomeJogador1;
+    private String nomeJogador2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,41 +26,52 @@ public class MainActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_main);
         inicializarVariaveis();
         btnContinuar.setOnClickListener(this);
+        imgGitHub.setOnClickListener(this);
     }
 
     private void inicializarVariaveis() {
         editText = findViewById(R.id.editText);
         btnContinuar = findViewById(R.id.btnContinuar);
         tvTitulo = findViewById(R.id.tvTitulo);
+        imgGitHub = findViewById(R.id.imgGitHub);
         cont = 0;
     }
 
     @Override
     public void onClick(View view) {
-        cont++;
-        switch (cont) {
-            case 1:
-                tvTitulo.setText("Nome do jogador 1");
-                tvTitulo.setTextSize(30);
-                editText.setVisibility(View.VISIBLE);
-                btnContinuar.setText("Continuar");
-                break;
-            case 2:
-                nomeJogador1 = editText.getText().toString();
-                editText.setText("");
-                tvTitulo.setText("Nome do jogador 2");
-                btnContinuar.setText("COMEÇAR!");
-                break;
-            case 3:
-                nomeJogador2 = editText.getText().toString();
-                finish();
-                Intent intent = new Intent(this, PerguntaActivity.class);
-                intent.putExtra("nomeJogador1",nomeJogador1);
-                intent.putExtra("nomeJogador2",nomeJogador2);
-                startActivity(intent);
-                break;
-            default:
-                finish();
+        if (view.getId() == R.id.imgGitHub) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Hirotsumi/ehindividual")));
+        }else{
+            cont++;
+            switch (cont) {
+                case 0:
+                    tvTitulo.setText("É individual?");
+                    tvTitulo.setTextSize(50);
+                    editText.setVisibility(View.GONE);
+                    break;
+                case 1:
+                    tvTitulo.setText("Nome do jogador 1");
+                    tvTitulo.setTextSize(30);
+                    editText.setVisibility(View.VISIBLE);
+                    btnContinuar.setText("Continuar");
+                    break;
+                case 2:
+                    nomeJogador1 = editText.getText().toString();
+                    editText.setText("");
+                    tvTitulo.setText("Nome do jogador 2");
+                    btnContinuar.setText("COMEÇAR!");
+                    break;
+                case 3:
+                    nomeJogador2 = editText.getText().toString();
+                    finish();
+                    Intent intent = new Intent(this, PerguntaActivity.class);
+                    intent.putExtra("nomeJogador1", nomeJogador1);
+                    intent.putExtra("nomeJogador2", nomeJogador2);
+                    startActivity(intent);
+                    break;
+                default:
+                    finish();
+            }
         }
     }
 
